588b735c732a914c0faddf3c8e982b8615ef3b89
james <ojameskola03@gmail.com>
2024-03-25 20:00:01 +0100

    new structure

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,20 +1,16 @@
 import streamlit as st
 # from st_audiorec import st_audiorec
 
-# DESIGN implement changes to the standard streamlit UI/UX
-# --> optional, not relevant for the functionality of the component!
-st.set_page_config(page_title="voice-thenticate")
-# Design move app further up and remove top padding
-st.markdown('''<style>.css-1egvi7u {margin-top: -3rem;}</style>''',
-            unsafe_allow_html=True)
-# # Design change st.Audio to fixed height of 45 pixels
-# st.markdown('''<style>.stAudio {height: 45px;}</style>''',
-#             unsafe_allow_html=True)
-# # Design change hyperlink href link color
-# st.markdown('''<style>.css-v37k9u a {color: #ff4c4b;}</style>''',
-#             unsafe_allow_html=True)  # darkmode
-# st.markdown('''<style>.css-nlntq9 a {color: #ff4c4b;}</style>''',
-#             unsafe_allow_html=True)  # lightmode
 
-# st.title("voice-thenticate")
-# st.description('')
+def voice_thenticate():
+            """
+            Displays GUI components for this demonstration
+            """          
+            # Display the title and info
+            st.title("voice-thenticate")
+            st.description('A practical application of speaker verification using SpeechBrains')
+
+
+if __name__ == '__main__':
+    # Call main function
+    voice_thenticate()
