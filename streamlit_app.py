336459463b47c7968fb6b337c72861fe5ca69611
james <ojameskola03@gmail.com>
2024-03-26 18:12:21 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,14 +39,6 @@ def voice_thenticate():
     # Section for recording initial user (owner) voice
     st.subheader("Set Up Your Voice ID")
 
-    # def callback_a():
-    #     """Plays back recorded audio associated with a given key from Streamlit session state.
-    #     """
-    #     if st.session_state.A_output:
-    #         # Access "bytes" key within data
-    #         audio_bytes_a = st.session_state.A_output["bytes"]
-    #         st.audio(audio_bytes_a)
-
     mic_recorder(
         start_prompt="Start recording ⏺️",
         stop_prompt="Stop recording ⏹️",
@@ -59,14 +51,6 @@ def voice_thenticate():
     # Section for recording user's voice for verification
     st.subheader("Unlock with Your Voice")
 
-    # def callback_b():
-    #     """Plays back recorded audio associated with a given key from Streamlit session state.
-    #     """
-    #     if st.session_state.B_output:
-    #         # Access "bytes" key within data
-    #         audio_bytes_b = st.session_state.B_output["bytes"]
-    #         st.audio(audio_bytes_b)
-
     mic_recorder(
         start_prompt="Start recording ⏺️",
         stop_prompt="Stop recording ⏹️",
@@ -75,25 +59,17 @@ def voice_thenticate():
         format="wav",
         key="B"
     )
-    if st.session_state.A_output is not None :
-        st.write(str(st.session_state.A_output["id"]))
-
-    if st.session_state.A_output is not None and st.session_state.B_output is not None:
-        st.write(str(st.session_state.A_output["id"]))
-        st.write(str(st.session_state.B_output["id"]))
 
     # Section for verifying user's voice with SpeechBrain
-    st.subheader("Verify Your Voice")
+    st.subheader("Verification Result")
 
     # "Verify" button with hover text
     if st.session_state.A_output is not None and st.session_state.B_output is not None:
-        st.button(
-            label="Verify",
-            key="C",
-            help="Match your voice sample to your enrolled voice ID",
-            on_click=verify(st.session_state.A_output["bytes"], st.session_state.B_output["bytes"]),
-            type="primary"
-        )
+        # Status message
+        st.write("verifying...")
+
+        # Verification outcone
+        verify(st.session_state.A_output["bytes"], st.session_state.B_output["bytes"])
 
 
 def verify(audio_a, audio_b) -> None:
@@ -104,20 +80,6 @@ def verify(audio_a, audio_b) -> None:
         audio_a: Bytes representing the enrolled user's voice sample.
         audio_b: Bytes representing the user's voice for verification.
     """
-    
-    verification = SpeakerRecognition.from_hparams(
-        source="speechbrain/spkrec-ecapa-voxceleb",
-        savedir="pretrained_models/spkrec-ecapa-voxceleb"
-    )
-        
-    # with tempfile.NamedTemporaryFile(suffix=".wav") as temp_file_a:
-    #     temp_file_a.write(audio_a)
-            
-    # with tempfile.NamedTemporaryFile(suffix=".wav") as temp_file_b:
-    #     temp_file_b.write(audio_b)
-    # with tempfile.NamedTemporaryFile(suffix=".wav") as temp_file_a, tempfile.NamedTemporaryFile(suffix=".wav") as temp_file_b:
-    #     temp_file_a.write(audio_a)
-    #     temp_file_b.write(audio_b)
     def save_audio_as_wav(audio_bytes, filename):
         with tempfile.NamedTemporaryFile(suffix=".wav", delete=False) as temp_file:
             temp_file.write(audio_bytes)
@@ -125,16 +87,21 @@ def verify(audio_a, audio_b) -> None:
 
     wav_path_a = save_audio_as_wav(audio_a, "audio_a.wav")
     wav_path_b = save_audio_as_wav(audio_b, "audio_b.wav")
-    score, prediction = verification.verify_files(wav_path_a, wav_path_b)
     
+    verification = SpeakerRecognition.from_hparams(
+        source="speechbrain/spkrec-ecapa-voxceleb",
+        savedir="pretrained_models/spkrec-ecapa-voxceleb"
+    )
+    
+    score, prediction = verification.verify_files(wav_path_a, wav_path_b)
 
     # Convert tensor prediction to boolean for conditional logic
     prediction_bool = prediction.item() == 1  # True if prediction is [True]
         
     if prediction_bool:
-        st.success("Voice verified successfully!")
+        st.success("✅ Voice verified successfully!")
     else:
-        st.error("Voice verification failed. Please try again.")
+        st.error("❌ Voice verification failed. Please try again.")
 
 
 if __name__ == "__main__":
