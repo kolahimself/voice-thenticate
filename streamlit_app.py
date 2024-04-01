b631181800fa65409444a2fbdb534a6d7972f586
james <ojameskola03@gmail.com>
2024-04-01 06:32:20 +0100

    fix syntax

--- streamlit_app.py
+++ streamlit_app.py
@@ -93,22 +93,20 @@ def display_initial_ui(reg_usernames: list) -> str:
     """
     display_initial_app_info()
 
-    
-    
     with st.empty():
         # Entry text field
         username = st.text_input(label="Username", key='A1')
 
     # Sign in & Sign up buttons
-    with st.empty()
-    col_left, col_right = placeholder.columns(2)
-    with col_left:
-        sign_in_button = placeholder.button(
-            label="Sign In",
-            key="A2",
-            type="primary",
-            use_container_width=True
-        )
+    with st.empty():
+        col_left, col_right = placeholder.columns(2)
+        with col_left:
+            sign_in_button = placeholder.button(
+                label="Sign In",
+                key="A2",
+                type="primary",
+                use_container_width=True
+            )
 
     # with col_right:
     #     sign_up_button = placeholder.button(
