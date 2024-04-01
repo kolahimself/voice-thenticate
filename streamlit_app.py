26929b9a387d067639ea6e543871ccf25772cc42
james <ojameskola03@gmail.com>
2024-04-01 06:22:39 +0100

    placeholder_trials

--- streamlit_app.py
+++ streamlit_app.py
@@ -118,7 +118,7 @@ def display_initial_ui(reg_usernames: list) -> str:
     
     if sign_in_button:
         # sign_in(username, reg_usernames)
-        pass
+        placeholder.empty()
     # elif sign_up_button:
     #     # sign_up()
     #     pass
