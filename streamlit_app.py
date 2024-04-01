80ea25ba7524b61a7fcb36366c251fb567e2ffb0
james <ojameskola03@gmail.com>
2024-04-01 06:58:37 +0100

    positional fix for sign up nd sign in buttons

--- streamlit_app.py
+++ streamlit_app.py
@@ -98,7 +98,7 @@ def display_initial_ui(reg_usernames: list) -> str:
     # Entry text field
     username = placeholders[0].text_input(label="Username", key='A1')
         
-    col_left, col_right = placeholders[1].columns(2)
+    col_left, col_right = st.columns(2)
         
     with col_left:
         sign_in_button = st.button(
