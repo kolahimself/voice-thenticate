e2c125783a8d7ed4cce64af5d74bcca246ae9173
james <ojameskola03@gmail.com>
2024-04-01 12:40:25 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -99,32 +99,32 @@ def display_initial_ui(reg_usernames: list, firebase_storage) -> str:
     def nextpage(): st.session_state.page += 1
     def restart(): st.session_state.page = 0
 
-    placeholder = st.empty()
-    st.button("Next",on_click=nextpage,disabled=(st.session_state.page > 3))
+    # placeholder = st.empty()
+    # st.button("Next",on_click=nextpage,disabled=(st.session_state.page > 3))
 
-    if st.session_state.page == 0:
-        # Replace the placeholder with some text:
-        placeholder.text(f"Hello, this is page {st.session_state.page}")
+    # if st.session_state.page == 0:
+    #     # Replace the placeholder with some text:
+    #     placeholder.text(f"Hello, this is page {st.session_state.page}")
 
-    elif st.session_state.page == 1:
-        # Replace the text with a chart:
-        placeholder.line_chart({"data": [1, 5, 2, 6]})
+    # elif st.session_state.page == 1:
+    #     # Replace the text with a chart:
+    #     placeholder.line_chart({"data": [1, 5, 2, 6]})
 
-    elif st.session_state.page == 2:
-        # Replace the chart with several elements:
-        with placeholder.container():
-            st.write("This is one element")
-            st.write("This is another")
-            st.metric("Page:", value=st.session_state.page)
+    # elif st.session_state.page == 2:
+    #     # Replace the chart with several elements:
+    #     with placeholder.container():
+    #         st.write("This is one element")
+    #         st.write("This is another")
+    #         st.metric("Page:", value=st.session_state.page)
 
-    elif st.session_state.page == 3:
-        placeholder.markdown(r"$f(x) = \exp{\left(x^🐈\right)}$")
+    # elif st.session_state.page == 3:
+    #     placeholder.markdown(r"$f(x) = \exp{\left(x^🐈\right)}$")
 
-    else:
-        with placeholder:
-            st.write("This is the end")
-            st.button("Restart",on_click=restart)
-    # display_initial_app_info()
+    # else:
+    #     with placeholder:
+    #         st.write("This is the end")
+    #         st.button("Restart",on_click=restart)
+    display_initial_app_info()
     
     # placeholders = [st.empty() for _ in range(2)]
         
@@ -334,6 +334,10 @@ if __name__ == "__main__":
 
     # Retrieve registered usernames
     registered_usernames = fetch_firebase_data(storage)
+
+    # Initial page state ("initial", "sign_in", or "sign_up")
+    if "page" not in st.session_state:
+    st.session_state.page = "initial"  
 
     # Display initail user interface
     display_initial_ui(registered_usernames, storage)
