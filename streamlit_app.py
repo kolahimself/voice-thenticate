3322b4a2a0e18e094a31653a678c916d8f8a221f
james <ojameskola03@gmail.com>
2024-04-01 03:55:55 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -55,7 +55,8 @@ def init_firebase_storage():
         'storageBucket': "voice-thenticate.appspot.com",
         'messagingSenderId': "583252692015",
         'appId': "1:583252692015:web:9615861360f2bcc69a8ada",
-        'measurementId': "G-KR9Z6EHF56"
+        'measurementId': "G-KR9Z6EHF56",
+        'serviceAccount': 'firebase-adminsdk-nsv2t@voice-thenticate.iam.gserviceaccount.com'
     }
 
     firebase = pyrebase.initialize_app(config)
